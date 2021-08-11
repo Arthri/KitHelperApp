01c13aa1575acfabf97db07777313c127afc1073
Arthri <41360489+Arthri@users.noreply.github.com>
2021-08-11 08:32:59 +0800

    feat: Add project files

---
+++ KitHelperApp/Program.cs
@@ -0,0 +1,162 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Terraria;
+using Terraria.ID;
+
+namespace KitHelperApp
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var lang = "en-US";
+            Console.WriteLine($"Fetching Item Names for locale: {lang}...");
+            var idMap = typeof(ItemID)
+                .GetFields(BindingFlags.Public | BindingFlags.Static)
+                .Where(f => f.IsLiteral && !f.IsInitOnly)
+                .ToDictionary(f => (short)f.GetValue(null), f => f.Name);
+            idMap.Remove(ItemID.None);
+            idMap.Remove(ItemID.Count);
+
+            var assembly = typeof(ItemID).Assembly;
+            var resource = Utils.ReadEmbeddedResource($"Terraria.Localization.Content.{lang}.Items.json");
+            var json = JsonConvert.DeserializeObject<Dictionary<string, Dictionary<string, string>>>(resource);
+            var resDict = new Dictionary<string, string>();
+            foreach (var ktp in json["ItemName"])
+            {
+                resDict.Add(ktp.Key, ktp.Value);
+            }
+
+            idMap = idMap.ToDictionary(i => i.Key, i =>
+            {
+                if (resDict.TryGetValue(i.Value, out var str))
+                {
+                    return str;
+                }
+                else
+                {
+                    Console.WriteLine($"No key for {i.Value}");
+                    return i.Value;
+                }
+            });
+            resDict = null;
+            json = null;
+
+
+            Console.WriteLine("Reading data file...");
+            var names = JsonConvert.DeserializeObject<string[]>(File.ReadAllText("name-def.json"));
+            var sb = new StringBuilder();
+            var sdata = JsonConvert.DeserializeObject<Dictionary<string, Kit[]>>(File.ReadAllText("data.json"));
+            var kits = sdata["latecomer-items"];
+            for (var i = 0; i < kits.Length; i++)
+            {
+                var kit = kits[i];
+                sb.AppendLine("# " + (i < names.Length ? names[i] : "Unnamed"));
+                if (kit.HP != null)
+                {
+                    sb.AppendLine(string.Format("**HP:** `{0}`", kit.HP));
+                }
+                if (kit.Mana != null)
+                {
+                    sb.AppendLine(string.Format("**MP:** `{0}`", kit.Mana));
+                }
+                if (kit.Items != null)
+                {
+                    PrintItemCollection(kit.Items, "Items");
+                }
+                if (kit.CorruptionItems != null)
+                {
+                    PrintItemCollection(kit.CorruptionItems, "Corruption Items");
+                }
+                if (kit.CrimsonItems != null)
+                {
+                    PrintItemCollection(kit.CrimsonItems, "Crimson Items");
+                }
+                if (kit.GimmickItems != null)
+                {
+                    foreach (var kvp in kit.GimmickItems)
+                    {
+                        PrintItemCollection(kvp.Value, $"{kvp.Key} Items");
+                    }
+                }
+
+                void PrintItem(KitItem item)
+                {
+                    string itemName;
+                    if (item.ID != null)
+                    {
+                        itemName = idMap[item.ID.Value];
+                    }
+                    else
+                    {
+                        itemName = "HOW DARE YOU PUT ITEM ID 0";
+                    }
+                    var result = $"- `{item.Probability * 100}%` {item.Stack ?? 0} `{itemName}`.";
+                    if (item.GiveFor > 0)
+                    {
+                        result += $" For {item.GiveFor} kits onward.";
+                    }
+                    sb.AppendLine(result);
+                }
+
+                void PrintItemCollection(KitItem[] items, string name)
+                {
+                    if (items.Length > 0)
+                    {
+                        sb.AppendLine($"## {name}");
+                        foreach (var item in items)
+                        {
+                            PrintItem(item);
+                        }
+                    }
+                }
+            }
+
+            File.WriteAllText("data.md", sb.ToString());
+        }
+    }
+
+    public class Kit
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("hp")]
+        public int? HP { get; set; }
+
+        [JsonProperty("mana")]
+        public int? Mana { get; set; }
+
+        [JsonProperty("items")]
+        public KitItem[] Items { get; set; }
+
+        [JsonProperty("corruption-items")]
+        public KitItem[] CorruptionItems { get; set; }
+
+        [JsonProperty("crimson-items")]
+        public KitItem[] CrimsonItems { get; set; }
+
+        [JsonProperty("gimmick-items")]
+        public Dictionary<string, KitItem[]> GimmickItems { get; set; }
+    }
+
+    public class KitItem
+    {
+        [JsonProperty("id")]
+        public short? ID { get; set; }
+
+        [JsonProperty("stack")]
+        public int? Stack { get; set; }
+
+        [JsonProperty("give-for")]
+        public int? GiveFor { get; set; }
+
+        [JsonProperty("probability")]
+        public double? Probability { get; set; }
+    }
+}
